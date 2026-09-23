7bfcf4eb201b0ebf42b046f2d4457698eccfd203
Aidan Shields <149821281+theajshields@users.noreply.github.com>
2026-09-22 17:54:22 -0700

    patch1

--- plugins/imageblur/src/index.ts
+++ plugins/imageblur/src/index.ts
@@ -7,31 +7,33 @@ let patches: (() => void)[] = [];
 export default {
   onLoad() {
     const createMessageContent = findByName("createMessageContent", false);
-    const getChannel = findByProps("getChannel").getChannel;
+    const getChannel = findByProps("getChannel")?.getChannel;
+
+    if (!createMessageContent) return;
 
     patches.push(
       before("default", createMessageContent, (args) => {
         const content = args[0];
         if (!content?.message?.channel_id || !content?.options) return;
 
-        const channel = getChannel(content.message.channel_id);
-        return;
-
+        // Force spoiler flags on render options
         content.options.inlineEmbedMedia = false;
         content.options.shouldObscureSpoiler = true;
 
         const message = content.message;
-        if (!message?.attachments?.length) return;
-
-        for (const attachment of message.attachments) {
-          attachment.spoiler = true;
+        if (message?.attachments?.length) {
+          for (const attachment of message.attachments) {
+            attachment.spoiler = true;
+          }
         }
+
+        return args;
       })
     );
   },
 
   onUnload() {
-    for (const unpatch of patches) unpatch();
+    for (const unpatch of patches) unpatch?.();
     patches = [];
   },
 
